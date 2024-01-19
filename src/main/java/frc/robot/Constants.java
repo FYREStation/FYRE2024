e85f5145b46a4db247102b794580a5cf02d74802
Ian G <66448861+iangaunt@users.noreply.github.com>
2024-01-18 18:52:25 -0800

    Added Checkstyle formatting file

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,7 +13,8 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-	public static class OperatorConstants {
-		public static final int kDriverControllerPort = 0;
-	}
+    /** Initializes the operator constants. */
+    public static class OperatorConstants {
+        public static final int kDriverControllerPort = 0;
+    }
 }
