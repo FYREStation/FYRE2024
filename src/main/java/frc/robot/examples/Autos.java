e2b0567e6bf0fcb7e5fb6c76588c969c206156b2
Ian G <66448861+iangaunt@users.noreply.github.com>
2024-01-21 13:49:00 -0800

    Moved the Autos.java file

--- src/main/java/frc/robot/examples/Autos.java
+++ src/main/java/frc/robot/examples/Autos.java
@@ -6,7 +6,6 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import frc.robot.subsystems.ExampleSubsystem;
 
 /** The class for containing autonomous functionality. */
 public final class Autos {
